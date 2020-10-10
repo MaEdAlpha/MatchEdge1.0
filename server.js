f425348f3cce04f136637079aa660bfaedae64e9
MaEdAlpha <72553021+MaEdAlpha@users.noreply.github.com>
2020-10-10 23:49:20 +0000

    Import nodemon
    allows auto server restart

--- server.js
+++ server.js
@@ -1,10 +1,61 @@
 //need to import http package from node.js
-const http = require('http'); //default node.js package already installed on node.js
 const app = require('./backend/app');
+const http = require('http'); //default node.js package already installed on node.js
+const debug = require('debug')('node-angular');
+// const { debug } = require('console');
 
-const port = process.env.PORT || 3000; //process.env dyanmically  variables part of node features. Always used for listening on ports
+
+const normalizePort = val => {
+  var port = parseInt(val,10);
+
+  if(isNaN(port)) {
+    //name pipe
+    return val;
+  }
+
+  if (port >=0){
+    //port number
+    return port;
+  }
+
+  return false;
+};
+
+const onError = error => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " require eleveated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+  debug("Listening on " + bind);
+};
+
+const port = normalizePort(process.env.PORT || 3000); //process.env dyanmically  variables part of node features. Always used for listening on ports
 
 app.set('port', port);
+
+//create a server constant (with app express imports and used as an object in createServer() )
+const server = http.createServer(app);
+server.on("error", onError);
+server.on("listening", onListening);
+//set port you want to host server at. for now, we do localhost 3000
+server.listen(port);
 
 //create a server constant (without app express. Example of the most basic server)
 
@@ -12,10 +63,8 @@ app.set('port', port);
                           //   res.end('My First Response');
                           // });
 
-//create a server constant (with app express imports and used as an object in createServer() )
-const server = http.createServer(app);
 
-//set port you want to host server at. for now, we do localhost 3000
-server.listen(port);
+
+
 
 
