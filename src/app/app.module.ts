12fc0c807eb631563c5e148b701c1e1c878cc543
MaEdAlpha <72553021+MaEdAlpha@users.noreply.github.com>
2021-07-06 09:36:24 +0100

    UI - Watchlist
    Styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -110,6 +110,7 @@ import { PopupContactUsComponent } from './popup-contact-us/popup-contact-us.com
 import { TermsOfUseComponent } from './terms-of-use/terms-of-use.component';
 import { PrivacyContentComponent } from './privacy-content/privacy-content.component';
 import { PopupDataProtectionRegulationComponent } from './popup-data-protection-regulation/popup-data-protection-regulation.component';
+import { PopupCookiePolicyComponent } from './popup-cookie-policy/popup-cookie-policy.component';
 
 
 
@@ -176,6 +177,7 @@ import { PopupDataProtectionRegulationComponent } from './popup-data-protection-
     TermsOfUseComponent,
     PrivacyContentComponent,
     PopupDataProtectionRegulationComponent,
+    PopupCookiePolicyComponent,
   ],
   imports: [
     AppRoutingModule,
---
+++ src/app/popup-cookie-policy/popup-cookie-policy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-popup-cookie-policy',
+  templateUrl: './popup-cookie-policy.component.html',
+  styleUrls: ['./popup-cookie-policy.component.css']
+})
+export class PopupCookiePolicyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
