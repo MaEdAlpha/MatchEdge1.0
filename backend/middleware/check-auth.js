0beed4ae623a960c86de956a1ae9e2d2d412e577
MaEdAlpha <72553021+MaEdAlpha@users.noreply.github.com>
2021-07-30 20:39:19 +0700

    Landing Page Updates
    Styled CSS

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -7,9 +7,9 @@ module.exports = (req, res, next) => {
     // console.log(req.headers.authorization);
     const token = req.headers.authorization.split(" ")[1];
     // console.log(token);
-    console.log('Auth Check triggered');
+    console.log('Auth Check triggered ' + process.env.JWT_PVT_K);
     //token  + private Key JWT_KEY
-    jwt.verify(token, "0A6aKFksbmPgDlIKiUGcMm82eycRgTivqkZx4zjDJn2CWm9LF5Kq5wnKltq4Uk3Zlpt9UJxbf");
+    jwt.verify(token, process.env.JWT_PVT_K);
     next();
   }catch (e) {
     res.status(401).json({message: "Auth Failed!"});
