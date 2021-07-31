20d7ed8263b0fd68390d04b042efe292e896807c
MaEdAlpha <72553021+MaEdAlpha@users.noreply.github.com>
2021-07-31 14:47:42 +0700

    CSS  for Subscribe section
    ON TEST ENV not LIVE DB

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-landing-page',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
-  constructor() { }
+  constructor( private dialog: MatDialog) { }
 
   ngOnInit(): void {
+  }
+
+  displayTwoUpInfo(){
+
   }
 
 }
